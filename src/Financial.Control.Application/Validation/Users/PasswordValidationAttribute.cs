79062913b9926a95fbb9585aaa3a07d7dbaf2a13
Lothar Matthaus <lotharmatt@alu.ufc.br>
2023-07-16 14:58:05 -0300

    Alteração na expressão regular de validação de senha.

--- src/Financial.Control.Application/Validation/Users/PasswordValidationAttribute.cs
+++ src/Financial.Control.Application/Validation/Users/PasswordValidationAttribute.cs
@@ -7,7 +7,7 @@ namespace Financial.Control.Application.Validation.Users
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$";
+            string pattern = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?])[A-Za-z\d-+_!@#$%^&*.,?]{8,}$";
 
             if (!Regex.IsMatch(value as string, pattern))
                 return new ValidationResult("A senha deve conter: Pelo menos 8 dígitos. Um número. Uma letra minúscula. Uma letra maiúscula. Um caractere especial.", new List<string> { validationContext.MemberName });
